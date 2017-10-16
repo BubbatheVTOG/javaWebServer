808bfb66d57400286d4d9506d611c0192af65d7d
bubbathevtog <bubbathevtog@gmail.com>
2017-10-16 07:21:39 -0400

    added liscenses

--- TinyHTTPd.java
+++ TinyHTTPd.java
@@ -25,6 +25,24 @@ import java.text.*;
  *	8) We send out our respose back to the client.
  *	9) The ClientConnection has ran its course and is now finished.
  *	10) Done.
+ *
+ * A Java HTTP Server
+ * Copyright (C) 2017 Zachary "Bubba" Lichvar
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 
 public class TinyHTTPd{
